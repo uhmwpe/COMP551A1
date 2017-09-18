2a3667c3336b4fc8537a6eacfacff9612dc65cab
Isaac Chan <isaac.chan@mail.mcgill.ca>
2017-09-17 20:17:51 -0400

    fixed regex mistake

--- ExtractionScript.py
+++ ExtractionScript.py
@@ -54,7 +54,7 @@ for n in re.findall(r'[^\u4e00-\u9fff，。／【】、；‘:""]+',df.iloc[2].c
 # In[6]:
 
 df['commentText'] = df['commentText'].str.replace(r"[^\u4e00-\u9fff，。／【】、；‘:""]+","")
-df['repliesCommentText'] = df['repliesCommentText'].str.replace(r"[^\u4e00-\u9fff，。／【】、；‘:""]+","")
+df['repliesCommentText'] = df['repliesCommentText'].str.replace(r"[^\u4e00-\u9fff，。／【】、；‘:""\d]+","")
 
 
 
