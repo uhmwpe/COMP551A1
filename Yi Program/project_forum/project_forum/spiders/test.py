340a451e15e0cea4195f6321e09a81f32c7a5ff5
wornbb <wornbb@gmail.com>
2017-09-19 01:31:39 -0400

    Progress: Comment sections are now exposed to the robot. 2 more steps away.

--- Yi Program/project_forum/project_forum/spiders/test.py
+++ Yi Program/project_forum/project_forum/spiders/test.py
@@ -1,19 +1,7 @@
-import scrapy
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+#...
+#from selenium import webdriver
 
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-
+#browser = webdriver.Chrome('C:/Users/Yi/PycharmProjects/COMP551A1/Yi Program/project_forum/project_forum/spiders/chromedriver.exe')
+#browser.get('https://www.zhihu.com/')
+#browser.save_screenshot('screenie.png')
+#...
